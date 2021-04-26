9850a86763d8110319777debee743f53f1bbfcec
bman6482 <83172999+bman6482@users.noreply.github.com>
2021-04-26 11:39:59 +0100

    Update test_filter_sort_customers.py

--- test_filter_sort_customers.py
+++ test_filter_sort_customers.py
@@ -6,19 +6,19 @@ from filter_sort_customers import Customers
 class TestCustomers(unittest.TestCase):
     def test_customers_filtering(self):
         """
-        Test that it can filter and sort customers
+        Test that script can filter and sort customers
         """
-        customer_list_file = '/data2/input_data/intercom/customers.txt'
+        customer_list_file = 'customers.txt'
         office_location = '53.339428, -6.257664'
         distance_range = 100
-        expected_customer_list_size = 16
         customers = Customers()
         customers.load_from_file(customer_list_file)
         coordinates = Customers.validate_extract_location(office_location)
         customers.filter_by_distance(coordinates, distance_range)
         customers.sort_by_id()
-        customer_list_size = len(customers.customer_list)
-        self.assertEqual(customer_list_size, expected_customer_list_size)
+        self.assertEqual(len(customers.customer_list), 16)
+        self.assertEqual(customers.customer_list[0]['user_id'], 4)
+        self.assertEqual(customers.customer_list[15]['user_id'], 39)
 
 
 if __name__ == '__main__':
